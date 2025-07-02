6af7e32cdc3b0c2f2613fc3f879ad1f7782091e1
BAHD <31705570+BartuAbiHD@users.noreply.github.com>
2025-07-02 03:39:33 +0300

    Update OPList.java

--- src/com/bartuabihd/oplist/OPList.java
+++ src/com/bartuabihd/oplist/OPList.java
@@ -6,45 +6,68 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 
 public class OPList extends JavaPlugin {
 	
+	OPList plugin;
+	
+	int resourceID;
+    
+    public OPList() {
+        this.resourceID = 126579;
+        
+    }
+	
 	public void onEnable()
 	{
-		getLoggerColored(ChatColor.GREEN + "Plugin is enabled!");
+		this.getLoggerColored(ChatColor.GREEN + "Plugin is enabled!");
+		
+		new com.bartuabihd.oplist.UpdateChecker(this, this.resourceID).getLatestVersion(version -> {
+            if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                this.getLoggerColored(ChatColor.GRAY + "[" + ChatColor.BLUE + "OperatorsList" + ChatColor.GRAY + "] " + ChatColor.RED + "A new update of the plugin is not available.");
+            }
+            else {
+                this.getLoggerColored(ChatColor.GRAY + "[" + ChatColor.BLUE + "OperatorsList" + ChatColor.GRAY + "] " + ChatColor.GREEN + "The plugin has a new update. Download link: http://bit.ly/mcpldownloadermc");
+            }
+        });
 	}
 	
 	public void onDisable()
 	{
-		getLoggerColored(ChatColor.RED + "Plugin is disabled!");
+		this.getLoggerColored(ChatColor.RED + "Plugin is disabled!");
 	}
 	
-	public boolean onCommand(CommandSender sender,Command komut,String label, String[] args)
-	{
-		if(komut.getName().equalsIgnoreCase("oplist") || komut.getName().equalsIgnoreCase("op list")){
-			Set<OfflinePlayer> list = (Bukkit.getOperators());
-		     
-			if (sender.hasPermission("operators.list")){
-				if(args.length == 0){
-					sender.sendMessage(ChatColor.YELLOW + "Operators:");
-					for (OfflinePlayer p : list) {
-						sender.sendMessage("   - " + p.getName());
-					}
-					//sender.sendMessage("Usage /oplist");
-		 	} else if(args.length >= 1){
-		 		
-		 		if(args[0].equalsIgnoreCase("about")){
-		 			sender.sendMessage(ChatColor.GREEN + "Coder by " + ChatColor.RED + "BartuAbiHD");
-		 			sender.sendMessage(ChatColor.GREEN + "Version: " + ChatColor.RED + this.getDescription().getVersion());
-		 		}
-		 		
-	 		}
-				
-		 }
-	  }
-	  return true;
+	public boolean onCommand(CommandSender sender, Command komut, String label, String[] args) {
+	    // Hem /oplist hem de /op list için kontrol
+	    if (komut.getName().equalsIgnoreCase("oplist") || label.equalsIgnoreCase("list") || komut.getName().equalsIgnoreCase("op list")) {
+	        Set<OfflinePlayer> list = (Bukkit.getOperators());
+	         
+	        if (sender.hasPermission("operators.list")) {
+	            if (args.length == 0 || (args.length == 1 && args[0].equalsIgnoreCase("about"))) {
+	                if (args.length == 0) {
+	                    sender.sendMessage(ChatColor.YELLOW + "Operators:");
+	                    for (OfflinePlayer p : list) {
+	                        sender.sendMessage("   - " + p.getName());
+	                    }
+	                } else if (args[0].equalsIgnoreCase("about")) {
+	                    sender.sendMessage(ChatColor.GREEN + "Coder by " + ChatColor.RED + "BartuAbiHD");
+	                    sender.sendMessage(ChatColor.GREEN + "Version: " + ChatColor.RED + this.getDescription().getVersion());
+	                }
+	            } else {
+	                // Geçersiz argüman durumu
+	                return false;
+	            }
+	        } else {
+	            // Yetki yok
+	            sender.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
+	            return true;
+	        }
+	        return true;
+	    }
+	    return false;
 	}
 	
 	private void getLoggerColored(String msg) {
