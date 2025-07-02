043cb2241cae47c8200f41677778217becb6957a
BAHD <31705570+BartuAbiHD@users.noreply.github.com>
2025-07-02 03:40:49 +0300

    Add files via upload

---
+++ src/com/bartuabihd/oplist/UpdateChecker.java
@@ -0,0 +1,32 @@
+package com.bartuabihd.oplist;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+import org.bukkit.Bukkit;
+public class UpdateChecker {
+
+    private com.bartuabihd.oplist.OPList plugin;
+    private int resourceId;
+
+    public UpdateChecker(com.bartuabihd.oplist.OPList plugin, int resourceId) {
+        this.plugin = plugin;
+        this.resourceId = resourceId;
+    }
+
+    public void getLatestVersion(final Consumer<String> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream();
+                 Scanner scanner = new Scanner(inputStream)) {
+            if (scanner.hasNext()) {
+                consumer.accept(scanner.next());
+            }
+        } catch (IOException exception) {
+                this.plugin.getLogger().info("[OperatorsList] Cannot call updates: " + exception.getMessage());
+            }
+        });
+    }
+}
